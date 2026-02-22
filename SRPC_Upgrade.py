93eaf99d3c529a290572cd36bea9c6acc9ef2136
chellachandar <chella.chandar@gmail.com>
2026-02-22 16:19:28 +0530

    Update SRPC_Upgrade.py

--- SRPC_Upgrade.py
+++ SRPC_Upgrade.py
@@ -295,12 +295,9 @@ if cfg_file and dat_file:
         
         st.markdown(
     """
-    ---
     ⚠️ **Performance Calculation Disclaimer**
-
     The accuracy and availability of protection performance parameters depend entirely on the 
-    disturbance recorder configuration within the relay.  
-    Missing pickup/start, trip, or breaker auxiliary digital signals will result in incomplete 
+    disturbance recorder configuration within the relay. Missing pickup/start, trip, or breaker auxiliary digital signals will result in incomplete 
     or unavailable timing calculations.
     """
 )
