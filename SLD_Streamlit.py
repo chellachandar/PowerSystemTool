9aeb1c032ba15164684eb79486f061620aa8731b
chellachandar <chella.chandar@gmail.com>
2026-02-24 21:50:50 +0530

    Update SLD_Streamlit.py

--- SLD_Streamlit.py
+++ SLD_Streamlit.py
@@ -604,9 +604,9 @@ if st.button("Generate AutoCAD DXF", type="primary"):
                 L = get_labels_15(feeder_num, i)
                 # --- PERFECTED SYMMETRIC DUAL CT ARCHITECTURE ---
                 # Upper Isolator
-                draw_isolator(ax, x_offset+.25, y_offset+0.6, L["base_isolator"], fs)
+                draw_isolator(ax, x_offset+.25, y_offset+0.4, L["base_isolator"], fs)
                 # Upper Earth Switch
-                earth_sh(ax, x_offset+.25, y_offset+0.1, L["earth_lbl1"], fs)
+                earth_sh(ax, x_offset+.25, y_offset+0.2, L["earth_lbl1"], fs)
                 
                 # TOP CT (ACT)
                 ct_a_lbl = L["ct_lbl"].replace("CT", "ACT")
@@ -620,9 +620,9 @@ if st.button("Generate AutoCAD DXF", type="primary"):
                 draw_ct(ax, x_offset+0.25, y_offset-3.8, ct_b_lbl, fs)  
                 
                 # Lower Earth Switch
-                earth_sh(ax, x_offset+0.25, y_offset-4.7, L["earth_lbl2"], fs)
+                earth_sh(ax, x_offset+0.25, y_offset-4.8, L["earth_lbl2"], fs)
                 # Lower Isolator
-                draw_isolator(ax, x_offset+0.25, y_offset-5.2, L["base_isolatorb"], fs)
+                draw_isolator(ax, x_offset+0.25, y_offset-5.0, L["base_isolatorb"], fs)
                 
                 # Continuous connecting wire
                 ax.plot([x_offset+0.25, x_offset+0.25], [y_offset+0.6, y_offset-5.2], color='red', linewidth=0.5)
